37a6b410b6e11c4701ffb764bebcb3f2cf7a8789
codewithmokone <codewithmokone@gmail.com>
2024-11-12 14:43:01 +0200

    add logic for the start and reset button

--- index.js
+++ index.js
@@ -1,31 +1,78 @@
 // const playButton = document.querySelector('.play');
-const resetButton = document.getElementsByClassName('reset')[0];
-const startButton = document.getElementsByClassName('start')[0];
-const lapButton = document.getElementsByClassName('lap')[0];
+const resetButton = document.querySelector('.reset');
+const startButton = document.querySelector('.start');
+const lapButton = document.querySelector('.lap');
+const minute = document.querySelector('.minute');
+const second = document.querySelector('.second');
+const milliSecond = document.querySelector('.mSecond');
 
+let min;
+let sec;
+let mSec;
+let minCounter = 0;
+let secCounter = 0;
+let mSecCounter = 0;
 let isPlay = false;
+let isReset = false;
+
 
 const toggleButton = () => {
   resetButton.classList.remove('hidden');
   lapButton.classList.remove('hidden');
 };
 
+
 const start = () => {
-    if(!isPlay){
-        startButton.innerHTML = 'Stop';
+    if(!isPlay && !isReset){
+        startButton.innerText = 'Stop';
+        min = setInterval(() => {
+            // if(minCounter === 60){
+            //     minCounter = 0;
+            // }
+            minute.innerText = `${++minCounter}.`;
+        }, 60*1000)
+        sec = setInterval(() => {
+            if(secCounter === 60){
+                secCounter = 0;
+            }
+            second.innerText = `${++secCounter}.`;
+        }, 1000)
+        mSec = setInterval(() => {
+            if(mSecCounter === 100){
+                mSecCounter = 0;
+            }
+            milliSecond.innerText = `${++mSecCounter}`;
+        }, 10)
         isPlay = true;
+        isReset = true;
     }else{
-        startButton.innerHTML = 'Start';
-        isPlay = true;
+        startButton.innerText = 'Start';
+        clearInterval(min);
+        clearInterval(sec);
+        clearInterval(mSec);
+        isPlay = false;
+        isReset = false;
     }
   toggleButton();
 };
 
 
+const startSec = () => {
+    setInterval(() => {
+        second.innerText = sec++;
+    }, 1000)
+}
+
+
+// Reset the watch
 const reset = () => {
-    play();
+    isReset = true;
+    start();
     resetButton.classList.add('hidden');
     lapButton.classList.add('hidden');
+    minute.innerText = '0 :'
+    second.innerText = '0 .'
+    milliSecond.innerText = '0'
 }
 
 startButton.addEventListener('click', start);
