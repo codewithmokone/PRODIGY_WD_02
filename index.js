ee3239256ea37eaa20cf6dc502fb25cc60a58b24
codewithmokone <codewithmokone@gmail.com>
2024-11-12 15:09:34 +0200

    updated the logic for clear all button

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// const playButton = document.querySelector('.play');
 const resetButton = document.querySelector('.reset');
 const startButton = document.querySelector('.start');
 const lapButton = document.querySelector('.lap');
+const clearButton = document.querySelector('.lap-clear-button');
 const minute = document.querySelector('.minute');
 const second = document.querySelector('.second');
 const milliSecond = document.querySelector('.mSecond');
+const laps = document.querySelector('.laps');
 
 let min;
 let sec;
@@ -12,6 +13,7 @@ let mSec;
 let minCounter = 0;
 let secCounter = 0;
 let mSecCounter = 0;
+let lapItem = 0;
 let isPlay = false;
 let isReset = false;
 
@@ -26,9 +28,6 @@ const start = () => {
     if(!isPlay && !isReset){
         startButton.innerText = 'Stop';
         min = setInterval(() => {
-            // if(minCounter === 60){
-            //     minCounter = 0;
-            // }
             minute.innerText = `${++minCounter}.`;
         }, 60*1000)
         sec = setInterval(() => {
@@ -57,23 +56,42 @@ const start = () => {
 };
 
 
-const startSec = () => {
-    setInterval(() => {
-        second.innerText = sec++;
-    }, 1000)
-}
-
-
 // Reset the watch
 const reset = () => {
     isReset = true;
     start();
     resetButton.classList.add('hidden');
     lapButton.classList.add('hidden');
-    minute.innerText = '0 :'
-    second.innerText = '0 .'
-    milliSecond.innerText = '0'
+    minute.innerText = '0 :';
+    second.innerText = '0 .';
+    milliSecond.innerText = '0';
+}
+
+const lap = () => {
+    const li = document.createElement('li');
+    const number = document.createElement('span');
+    const timeStamp = document.createElement('span');
+
+    li.setAttribute('class', 'lap-item');
+    number.setAttribute('class', 'number');
+    timeStamp.setAttribute('class','time-stamp');
+
+    number.innerText = `${++lapItem}`;
+    timeStamp.innerText = `${minCounter} : ${secCounter} : ${mSecCounter}`;
+
+    li.append(number, timeStamp);
+    laps.append(li);
+
+    clearButton.classList.remove('hidden');
+}
+
+const clearAll = () => {
+    laps.innerHTML = '';
+    laps.append(clearButton);
+    clearButton.classList.add('hidden');
 }
 
 startButton.addEventListener('click', start);
-resetButton.addEventListener('click',reset)
+resetButton.addEventListener('click',reset);
+lapButton.addEventListener('click',lap);
+clearButton.addEventListener('click',clearAll);
